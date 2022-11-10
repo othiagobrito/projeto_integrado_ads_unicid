c101e534cd7d4695e10228a74eebe75c020f4cef
thiagobrito <thiago7brito@gmail.com>
2022-11-09 23:50:50 -0300

    HomeController with index view and app layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title') Home @endsection
+
+@section('styles')
+    
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('scripts')
+    
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('title')</title>
+
+        @yield('styles')
+    </head>
+
+    <body>
+        <header></header>
+
+        <main>
+            @yield('content')
+        </main>
+
+        <footer></footer>
+        
+        @yield('scripts')
+    </body>
+</html>
