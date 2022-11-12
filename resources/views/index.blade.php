7e92ba1d2a463e7b87f6f429998a244972a6c3f9
thiagobrito <thiago7brito@gmail.com>
2022-11-12 00:01:39 -0300

    Home Page completed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,67 @@
 @section('title') Home @endsection
 
 @section('styles')
-    
+    <link rel="stylesheet" href="{{ asset('css/general/style.css') }}">
 @endsection
 
 @section('content')
-    
+    <main class="m-auto">
+        <div class="card-group">
+            <div class="card">
+                <a href="" class="nav-link">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('imgs/person-fill-add.svg') }}" class="card-img-top" width="150px" height="150px">
+                        
+                        <div class="card-body">
+                            <h5 class="card-title">Cadastrar Funcionário</h5>
+                            <p class="card-text">Formulário para cadastrar novos funcionários da clínica.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="" class="nav-link">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('imgs/plus-circle-fill.svg') }}" class="card-img-top" width="150px" height="150px">
+                        
+                        <div class="card-body">
+                            <h5 class="card-title">Agendar atendimento</h5>
+                            <p class="card-text">Formulário de agendamento de atendimento.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card-group">
+            <div class="card">
+                <a href="" class="nav-link">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('imgs/person-fill-add.svg') }}" class="card-img-top" width="150px" height="150px">
+                        
+                        <div class="card-body">
+                            <h5 class="card-title">Cadastrar Cliente</h5>
+                            <p class="card-text">Formulário de cadastro de clientes da clínica.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="" class="nav-link">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('imgs/building-fill.svg') }}" class="card-img-top" width="150px" height="150px">
+                        
+                        <div class="card-body">
+                            <h5 class="card-title">Dashboard</h5>
+                            <p class="card-text">Todos os dados financeiros da clínica.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </main>
 @endsection
 
 @section('scripts')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,41 @@
         <title>@yield('title')</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
         @yield('styles')
     </head>
 
     <body>
-        <header></header>
+        @if (request()->route()->getName() !== 'login.index')
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <nav class="navbar navbar-expand-lg fixed-top theme-color-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">PONTO ESTÉTICA</a>
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Agenda</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Redes Sociais</a>
+                                </li>
+                            </u>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        @endif
 
         @yield('content')
 
@@ -26,7 +55,6 @@
         </footer>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
         @yield('scripts')
     </body>
 </html>
